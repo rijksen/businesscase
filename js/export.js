34999853c432853f1717aa680f286e73add95cc0
JohnRijksen <johnrijksen@innius.com>
2025-12-12 11:40:54 +0100

    Environmental impact added to report

--- js/export.js
+++ js/export.js
@@ -37,76 +37,71 @@ function exportPDF() {
     doc.setFont("helvetica", "normal");
 
     function addSection(title, values) {
-    // Subsection title with light background
-    doc.setFillColor(...lightGray);
-    doc.rect(14, y - 4, 182, 8, "F");
+        // Subsection title with light background
+        doc.setFillColor(...lightGray);
+        doc.rect(14, y - 4, 182, 8, "F");
 
-    doc.setFont("helvetica", "bold");
-    doc.text(title, 16, y + 2);
-    y += 8;
+        doc.setFont("helvetica", "bold");
+        doc.text(title, 16, y + 2);
+        y += 8;
 
-    doc.setFont("helvetica", "normal");
-    values.forEach(([label, val]) => {
-        // Check if this is a total line → then draw the addition line
-        const isTotaal =
-            label.toLowerCase().includes("totaal besparingspotentieel");
+        doc.setFont("helvetica", "normal");
+        values.forEach(([label, val]) => {
+            // Check if this is a total line → then draw the addition line
+            const isTotaal =
+                label.toLowerCase().includes("totaal besparingspotentieel");
 
-        if (isTotaal) {
-            // Addition line above the line
-            doc.setDrawColor(0, 0, 0);
-            doc.setLineWidth(0.2);
-            doc.line(160, y - 4, 196, y - 4); // Addition line above amount
-            doc.setFont("helvetica", "bold");
-        } else {
-            doc.setFont("helvetica", "normal");
-        }
+            if (isTotaal) {
+                // Addition line above the line
+                doc.setDrawColor(0, 0, 0);
+                doc.setLineWidth(0.2);
+                doc.line(160, y - 4, 196, y - 4); // Addition line above amount
+                doc.setFont("helvetica", "bold");
+            } else {
+                doc.setFont("helvetica", "normal");
+            }
 
-        doc.text(label, 18, y);
-        doc.text(val, 192, y, { align: "right" });
-        y += 6;
-    });
+            doc.text(label, 18, y);
+            doc.text(val, 192, y, { align: "right" });
+            y += 6;
+        });
 
-    doc.setDrawColor(...accentColor)
-    y += 4;
-}
+        doc.setDrawColor(...accentColor)
+        y += 4;
+    }
 
     // ======= Customer entered data =======
     addSection("Elektriciteit", [
-        ["Jaar volume (kWh)", document.getElementById("param_elec_vol").textContent],
-        ["Elektriciteit tarief (€/kWh)", document.getElementById("param_elec_inv").textContent],
-        ["Totaal Elektriciteit", document.getElementById("elec_total").textContent]
+        ["Jaarverbruik elektriciteit (kWh)", document.getElementById("param_elec_vol").textContent],
+        ["Elektriciteitstarief (€/kWh)", document.getElementById("param_elec_inv").textContent],
+        ["Jaarlijkse elektriciteitskosten (€)", document.getElementById("elec_total").textContent]
     ]);
 
     addSection("Gas", [
-        ["Jaar volume (m³)", document.getElementById("param_gas_vol").textContent],
-        ["Gas tarief (€/m³)", document.getElementById("param_gas_inv").textContent],
-        ["Totaal Gas", document.getElementById("gas_total").textContent]
+        ["Jaarverbruik gas (m³)", document.getElementById("param_gas_vol").textContent],
+        ["Gastarief (€/m³)", document.getElementById("param_gas_inv").textContent],
+        ["aarlijkse gaskosten (€)", document.getElementById("gas_total").textContent]
     ]);
 
     addSection("Onderhoud", [
-        ["Onderhoudskosten", document.getElementById("param_maint_cost").textContent]
+        ["Jaarlijkse onderhoudskosten (€)", document.getElementById("param_maint_cost").textContent]
     ]);
 
     addSection("Productievolume", [
-        ["Totaal volume (t)", document.getElementById("param_prod_vol").textContent]
+        ["Jaarproductievolume (t)", document.getElementById("param_prod_vol").textContent]
     ]);
-
-    const param_elec = document.getElementById("param_elec").textContent;
-    const param_gas =  document.getElementById("param_gas").textContent;
-    const param_maint = document.getElementById("param_maint").textContent;
 
     addSection("Ingestelde besparingsparameters", [
-        ["Elektriciteit", param_elec],
-        ["Gas", param_gas],
-        ["Onderhoud", param_maint]
+        ["Elektriciteit", document.getElementById("param_elec").textContent],
+        ["Gas", document.getElementById("param_gas").textContent],
+        ["Onderhoud", document.getElementById("param_maint").textContent]
     ]);
-
 
     // ======= Savings results =======
     doc.setFont("helvetica", "bold");
     doc.setFontSize(12);
     doc.setTextColor(...accentColor);
-    doc.text("Besparingsresultaten", 14, y);
+    doc.text("Besparingsresultaten op jaarbasis", 14, y);
     y += 4;
     doc.setLineWidth(0.2);
     doc.line(14, y, 196, y);
@@ -122,29 +117,46 @@ function exportPDF() {
         ["Totaal besparingspotentieel", document.getElementById("grand_total").textContent]
     ]);
 
+    const elc_c02 = parseTonumber(document.getElementById("kwh_save").textContent) * 0.35
+    const gas_c02 = parseTonumber(document.getElementById("m3_save").textContent) * 1.79
+    const total_c02 = formatMeasure((elc_c02 + gas_c02),"Kg",0)
+
+    addSection("Positive Millieu impact", [
+        ["Besparing gasverbruik", document.getElementById("m3_save").textContent],
+        ["Besparing electriciteitsverbruik", document.getElementById("kwh_save").textContent],
+        ["Vermindering CO2 uitstoot", String(total_c02)],
+    ]);
+
     // ======= KPI comparison =======
-    y += 6;
     const col1X = 14;
     const col2X = 115;
 
     const kpiOud = [
         ["Onderhoudskosten per ton", document.getElementById("maint_now").textContent],
-        ["Energie kosten per ton", document.getElementById("energy_now").textContent],
-        ["Gas verbuik (m³/ton) ", document.getElementById("m3_now").textContent],
-        ["Electriciteit verbruik (kWh/ton) ", document.getElementById("kwh_now").textContent]
+        ["Energiekosten per ton", document.getElementById("energy_now").textContent],
+        ["Gasverbuik (m³/ton) ", document.getElementById("m3_now").textContent],
+        ["Electriciteitsverbruik (kWh/ton) ", document.getElementById("kwh_now").textContent]
     ];
 
     const kpiNieuw = [
         ["Onderhoudskosten per ton", document.getElementById("maint_new").textContent],
         ["Energiekosten per ton", document.getElementById("energy_new").textContent],
-        ["Gas verbruik (m³/ton) ", document.getElementById("m3_new").textContent],
-        ["lectriciteit verbruik (kWh/ton) ", document.getElementById("kwh_new").textContent]
+        ["Gasverbruik (m³/ton) ", document.getElementById("m3_new").textContent],
+        ["Electriciteitsverbruik (kWh/ton) ", document.getElementById("kwh_new").textContent]
     ];
 
+    doc.setFillColor(...lightGray);
+    doc.rect(14, y - 4, 182, 8, "F");
+    doc.setFont("helvetica", "bold");
+    doc.text("Vergelijk kosten en verbruik", 16, y + 2);
+
+    y +=9;
+    
     // Column titles
     doc.setFont("helvetica", "bold");
     doc.text("Huidige kosten & verbruik", col1X, y);
     doc.text("Nieuwe kosten & verbruik", col2X, y);
+
     y += 6;
 
     doc.setFont("helvetica", "normal");
